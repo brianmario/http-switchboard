2d651013c46ebb459851b68a1bdac9ceba60389d
Brian Lopez <seniorlopez@gmail.com>
2008-11-17 23:35:11 -0800

    implemented initial round-robin operator, added some more logging in random operator

--- operators/round_robin_operator.rb
+++ operators/round_robin_operator.rb
@@ -8,6 +8,7 @@ class Operator
   
   def initialize
     @address_book = AddressBook.instance
+    @addresses = []
   end
   
   def configure(config)
@@ -17,14 +18,17 @@ class Operator
   # the purpose of this method is to return a *single* host for the backend connection
   # which is determined by this operators method of load-balancing
   def lookup_jack(data)
-    addresses = []
     jack = nil
     
     @@mutex.synchronize {
       addresses = @address_book.lookup_addresses(data)
+      
+      # union the arrays, removing duplicates
+      @addresses = @addresses | addresses
+      LOGGER.debug("Operator has #{@addresses.size} choice(s) to pick from - selecting the next one in round-robin order.")
+      # perform our robin
+      @addresses.push(jack = @addresses.shift)
     }
-    LOGGER.debug("Operator has #{addresses.size} choice(s) to pick from - selecting one at random.")
-    jack = addresses[rand(addresses.size)] if addresses.any?
     return jack
   end
 end
